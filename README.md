a2ecdfb48ac3d251b496bc1e45e8961b6626b02b
Arturo <arturotinajas@gmail.com>
2025-07-30 23:17:18 +0200

    Chngs

---
+++ README.md
@@ -0,0 +1,15 @@
+# Frontend Mentor - Blog preview card solution
+
+This is a solution to the [Contact form challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/contact-form--G-hYlqKJj).
+
+### Links
+
+- Solution URL: [Contact Form Code](https://github.com/Arth97/frontendmentor-contact-form)
+- Live Site URL: [FrontendmentorContactForm.io](https://arth97.github.io/frontendmentor-contact-form/)
+
+### Built with
+
+- HTML5
+- CSS
+- Tailwind
+- JavaScript
--- main.js
+++ main.js
@@ -1,3 +1,22 @@
-document.getElementById('btn').addEventListener('click', function() {
-  document.getElementById('mensaje').textContent = '¡Has hecho clic en el botón!';
+document.addEventListener('DOMContentLoaded', function() {
+  const form = document.getElementById('contactForm');
+  form.addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const nombre = form.elements['firstname'].value.trim();
+    const email = form.elements['email'].value.trim();
+
+    if (nombre === '' || email === '') {
+      alert('Por favor, completa todos los campos.');
+    } else if (!validateEmail(email)) {
+      alert('Por favor, ingresa un email válido.');
+    } else {
+      alert('¡Formulario enviado correctamente!');
+      // form.submit();
+    }
+  });
+
+  function validateEmail(email) {
+    return /\S+@\S+\.\S+/.test(email);
+  }
 });
